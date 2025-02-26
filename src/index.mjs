4e3572c7e45a90232038b2633dae68e0a65a2eb6
Rafael Scoralick <rafaelscoralick@gmail.com>
2025-02-26 20:41:20 -0300

    teste 1

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import router from './routes/route.mjs';
+import router from './../routes/route.mjs';
 import swaggerUI from 'swagger-ui-express';
-import swaggerFile from './../swagger-output.json' with {type: 'json'};
-import config from './config/config.mjs';
+import swaggerFile from './../../swagger-output.json' with {type: 'json'};
+import config from './../config/config.mjs';
 import cookieParser from 'cookie-parser';
+import { createServerlessHandler } from '@vercel/node';
 
 const app = express();
 app.use(cookieParser());
@@ -14,12 +15,15 @@ app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 app.get("/", (req, res) => { res.redirect('/docs'); });
 app.use("/", router);
 
-mongoose
-  .connect(
-    `mongodb+srv://${config.DB_USER}:${config.DB_PASSWORD}@cluster0.rczok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-  )
-  .then(() => {
-    app.listen(3000);
-    console.log("Conectou ao banco com sucesso");
-  })
-  .catch((error) => console.log(error));
+// Conectar ao MongoDB apenas uma vez
+const connectDB = async () => {
+  if (!mongoose.connection.readyState) {
+    await mongoose.connect(
+      `mongodb+srv://${config.DB_USER}:${config.DB_PASSWORD}@cluster0.rczok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    console.log("Conectado ao banco com sucesso");
+  }
+};
+connectDB();
+
+export default createServerlessHandler(app);
