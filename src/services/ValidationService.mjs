7fa1725e77fb521e97c8bf913cb3c13ac05ae4a8
Gustavo Motta Cardoso Silva <gustavomottadeveloper@gmail.com>
2025-05-20 01:02:09 -0300

    fix: reorder import statement for User model in ValidationService refactor: remove unnecessary builds section from vercel.json

--- src/services/ValidationService.mjs
+++ src/services/ValidationService.mjs
@@ -1,5 +1,5 @@
-import User from "../models/User.mjs";
 import jwt from "jsonwebtoken";
+import User from "../models/User.mjs";
 import config from "../config/config.mjs";
 
 export class UserValidationService {
