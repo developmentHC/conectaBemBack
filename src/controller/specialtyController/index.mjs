88a36406d5872ca7aa850e7f49492b102c757432
Ryan Nardelli <ryannardelli12@gmail.com>
2026-04-10 10:37:00 -0300

    feat(63): Unificando especialidades de acordo com a demanda da tarefa e feito correção na documentação para manter consistência refs #63

--- src/controller/specialtyController/index.mjs
+++ src/controller/specialtyController/index.mjs
@@ -1,4 +1,4 @@
-export const getSpecialties = async (req, res) => {
+export const getSpecialties = async (_req, res) => {
   /*
   #swagger.tags = ['Specialties']
   #swagger.summary = 'Lista todas as especialidades'
@@ -7,28 +7,56 @@ export const getSpecialties = async (req, res) => {
   #swagger.responses[200] = {
     description: 'Lista de especialidades retornada com sucesso',
     schema: [
-      { id: "reiki", name: "Reiki" },
-      { id: "acupuntura", name: "Acupuntura" },
-      { id: "aromaterapia", name: "Aromaterapia" },
-      { id: "massoterapia", name: "Massoterapia" },
-      { id: "meditacao", name: "Meditação" },
-      { id: "yoga", name: "Yoga" },
+        { id: "acupuntura", name: "Acupuntura" },
+        { id: "aromaterapia", name: "Aromaterapia" },
+        { id: "arteterapia", name: "Arteterapia" },
+        { id: "biodanca", name: "Biodança" },
+        { id: "cardiologia", name: "Cardiologia" },
+        { id: "cromoterapia", name: "Cromoterapia" },
+        { id: "dermatologia", name: "Dermatologia" },
+        { id: "fisioterapia", name: "Fisioterapia" },
+        { id: "fitoterapia", name: "Fitoterapia" },
+        { id: "hipnoterapia", name: "Hipnoterapia" },
+        { id: "homeopatia", name: "Homeopatia" },
+        { id: "massoterapia", name: "Massoterapia" },
+        { id: "meditacao", name: "Meditação" },
+        { id: "musicoterapia", name: "Musicoterapia" },
+        { id: "osteopatia", name: "Osteopatia" },
+        { id: "pilates", name: "Pilates" },
+        { id: "quiropraxia", name: "Quiropraxia" },
+        { id: "reflexoterapia", name: "Reflexoterapia" },
+        { id: "reiki", name: "Reiki" },
+        { id: "yoga", name: "Yoga" }
     ]
   }
 
   #swagger.responses[500] = {
     description: 'Erro interno no servidor',
-    schema: { error: "Erro interno" }
+    schema: { error: "Erro interno no Servidor" }
   }
   */
 
   try {
     const specialties = [
-      { id: "reiki", name: "Reiki" },
       { id: "acupuntura", name: "Acupuntura" },
       { id: "aromaterapia", name: "Aromaterapia" },
+      { id: "arteterapia", name: "Arteterapia" },
+      { id: "biodanca", name: "Biodança" },
+      { id: "cardiologia", name: "Cardiologia" },
+      { id: "cromoterapia", name: "Cromoterapia" },
+      { id: "dermatologia", name: "Dermatologia" },
+      { id: "fisioterapia", name: "Fisioterapia" },
+      { id: "fitoterapia", name: "Fitoterapia" },
+      { id: "hipnoterapia", name: "Hipnoterapia" },
+      { id: "homeopatia", name: "Homeopatia" },
       { id: "massoterapia", name: "Massoterapia" },
       { id: "meditacao", name: "Meditação" },
+      { id: "musicoterapia", name: "Musicoterapia" },
+      { id: "osteopatia", name: "Osteopatia" },
+      { id: "pilates", name: "Pilates" },
+      { id: "quiropraxia", name: "Quiropraxia" },
+      { id: "reflexoterapia", name: "Reflexoterapia" },
+      { id: "reiki", name: "Reiki" },
       { id: "yoga", name: "Yoga" },
     ];
 
