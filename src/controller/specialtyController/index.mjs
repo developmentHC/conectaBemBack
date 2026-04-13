87823e8cd2ecb8a2da4639c42afb840afdd27127
Tarcísio Pedro <tarcisiopgs@gmail.com>
2026-04-13 19:33:43 -0300

    refactor: extrai SPECIALTIES para constante module-level e remove async desnecessário

--- src/controller/specialtyController/index.mjs
+++ src/controller/specialtyController/index.mjs
@@ -1,4 +1,27 @@
-export const getSpecialties = async (_req, res) => {
+export const SPECIALTIES = [
+  { id: "acupuntura", name: "Acupuntura" },
+  { id: "aromaterapia", name: "Aromaterapia" },
+  { id: "arteterapia", name: "Arteterapia" },
+  { id: "biodanca", name: "Biodança" },
+  { id: "cardiologia", name: "Cardiologia" },
+  { id: "cromoterapia", name: "Cromoterapia" },
+  { id: "dermatologia", name: "Dermatologia" },
+  { id: "fisioterapia", name: "Fisioterapia" },
+  { id: "fitoterapia", name: "Fitoterapia" },
+  { id: "hipnoterapia", name: "Hipnoterapia" },
+  { id: "homeopatia", name: "Homeopatia" },
+  { id: "massoterapia", name: "Massoterapia" },
+  { id: "meditacao", name: "Meditação" },
+  { id: "musicoterapia", name: "Musicoterapia" },
+  { id: "osteopatia", name: "Osteopatia" },
+  { id: "pilates", name: "Pilates" },
+  { id: "quiropraxia", name: "Quiropraxia" },
+  { id: "reflexoterapia", name: "Reflexoterapia" },
+  { id: "reiki", name: "Reiki" },
+  { id: "yoga", name: "Yoga" },
+];
+
+export const getSpecialties = (_req, res) => {
   /*
   #swagger.tags = ['Specialties']
   #swagger.summary = 'Lista todas as especialidades'
@@ -35,30 +58,7 @@ export const getSpecialties = async (_req, res) => {
     schema: { error: "Erro interno no Servidor" }
   }
   */
-  const specialties = [
-    { id: "acupuntura", name: "Acupuntura" },
-    { id: "aromaterapia", name: "Aromaterapia" },
-    { id: "arteterapia", name: "Arteterapia" },
-    { id: "biodanca", name: "Biodança" },
-    { id: "cardiologia", name: "Cardiologia" },
-    { id: "cromoterapia", name: "Cromoterapia" },
-    { id: "dermatologia", name: "Dermatologia" },
-    { id: "fisioterapia", name: "Fisioterapia" },
-    { id: "fitoterapia", name: "Fitoterapia" },
-    { id: "hipnoterapia", name: "Hipnoterapia" },
-    { id: "homeopatia", name: "Homeopatia" },
-    { id: "massoterapia", name: "Massoterapia" },
-    { id: "meditacao", name: "Meditação" },
-    { id: "musicoterapia", name: "Musicoterapia" },
-    { id: "osteopatia", name: "Osteopatia" },
-    { id: "pilates", name: "Pilates" },
-    { id: "quiropraxia", name: "Quiropraxia" },
-    { id: "reflexoterapia", name: "Reflexoterapia" },
-    { id: "reiki", name: "Reiki" },
-    { id: "yoga", name: "Yoga" },
-  ];
-
   res.set("Cache-Control", "public, max-age=86400");
 
-  return res.status(200).json(specialties);
+  return res.status(200).json(SPECIALTIES);
 };
