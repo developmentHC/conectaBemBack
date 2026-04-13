40a15e37bcd21c151fbe6b7a5c248e06a1f4f31b
Ryan Nardelli <ryannardelli12@gmail.com>
2026-04-13 12:20:54 -0300

    fix: Foi simplificado a função baseado no array estatico

--- src/controller/specialtyController/index.mjs
+++ src/controller/specialtyController/index.mjs
@@ -35,35 +35,30 @@ export const getSpecialties = async (_req, res) => {
     schema: { error: "Erro interno no Servidor" }
   }
   */
+  const specialties = [
+    { id: "acupuntura", name: "Acupuntura" },
+    { id: "aromaterapia", name: "Aromaterapia" },
+    { id: "arteterapia", name: "Arteterapia" },
+    { id: "biodanca", name: "Biodança" },
+    { id: "cardiologia", name: "Cardiologia" },
+    { id: "cromoterapia", name: "Cromoterapia" },
+    { id: "dermatologia", name: "Dermatologia" },
+    { id: "fisioterapia", name: "Fisioterapia" },
+    { id: "fitoterapia", name: "Fitoterapia" },
+    { id: "hipnoterapia", name: "Hipnoterapia" },
+    { id: "homeopatia", name: "Homeopatia" },
+    { id: "massoterapia", name: "Massoterapia" },
+    { id: "meditacao", name: "Meditação" },
+    { id: "musicoterapia", name: "Musicoterapia" },
+    { id: "osteopatia", name: "Osteopatia" },
+    { id: "pilates", name: "Pilates" },
+    { id: "quiropraxia", name: "Quiropraxia" },
+    { id: "reflexoterapia", name: "Reflexoterapia" },
+    { id: "reiki", name: "Reiki" },
+    { id: "yoga", name: "Yoga" },
+  ];
 
-  try {
-    const specialties = [
-      { id: "acupuntura", name: "Acupuntura" },
-      { id: "aromaterapia", name: "Aromaterapia" },
-      { id: "arteterapia", name: "Arteterapia" },
-      { id: "biodanca", name: "Biodança" },
-      { id: "cardiologia", name: "Cardiologia" },
-      { id: "cromoterapia", name: "Cromoterapia" },
-      { id: "dermatologia", name: "Dermatologia" },
-      { id: "fisioterapia", name: "Fisioterapia" },
-      { id: "fitoterapia", name: "Fitoterapia" },
-      { id: "hipnoterapia", name: "Hipnoterapia" },
-      { id: "homeopatia", name: "Homeopatia" },
-      { id: "massoterapia", name: "Massoterapia" },
-      { id: "meditacao", name: "Meditação" },
-      { id: "musicoterapia", name: "Musicoterapia" },
-      { id: "osteopatia", name: "Osteopatia" },
-      { id: "pilates", name: "Pilates" },
-      { id: "quiropraxia", name: "Quiropraxia" },
-      { id: "reflexoterapia", name: "Reflexoterapia" },
-      { id: "reiki", name: "Reiki" },
-      { id: "yoga", name: "Yoga" },
-    ];
+  res.set("Cache-Control", "public, max-age=86400");
 
-    res.set("Cache-Control", "public, max-age=86400");
-
-    return res.status(200).json(specialties);
-  } catch (_error) {
-    return res.status(500).json({ error: "Erro interno no servidor." });
-  }
+  return res.status(200).json(specialties);
 };
